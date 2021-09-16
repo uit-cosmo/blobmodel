1c9e56d641684a4d9254b6caa56f1eceb2023de8
Decristoforo <gregor.decristoforo@uit.no>
2021-09-16 13:35:27 +0200

    initial blob parameters

--- model/2d_blobs.py
+++ model/2d_blobs.py
@@ -1,6 +1,15 @@
 import numpy as np
 
 class Blob:
-	def __init__(self, size)
-		self.size = size
+	def __init__(self, id, amplitude, blob_shape, width_x, width_y, v_x, v_y, pos_x, pos_y, t_init ):
+		seld,id = id
+		self.amplitude = amplitude
+		self.blob_shape = blob_shape
+		self.width_x = width_x
+		self.width_y = width_y
+		self.v_x = v_x
+		self.v_y = v_y
+		self.pos_x = pos_x
+		self.pos_y = pos_y
+		self.t_init = t_init	
 
