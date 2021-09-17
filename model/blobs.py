655852c10562e883ee91f330181177a4f915d5f9
Decristoforo <gregor.decristoforo@uit.no>
2021-09-17 15:40:00 +0200

    change Blob to dataclass

--- model/blobs.py
+++ model/blobs.py
@@ -1,16 +1,13 @@
-import numpy as np
-from .model import Model
+from dataclasses import dataclass
 
-class Blob(Model):
-	def __init__(self, id, amplitude, blob_shape, width_x, width_y, v_x, v_y, pos_x, pos_y, t_init ):
-		seld,id = id
-		self.amplitude = amplitude
-		self.blob_shape = blob_shape
-		self.width_x = width_x
-		self.width_y = width_y
-		self.v_x = v_x
-		self.v_y = v_y
-		self.pos_x = pos_x
-		self.pos_y = pos_y
-		self.t_init = t_init	
-
+@dataclass
+class Blob():
+	id: int
+	amplitude: float
+	width_x: float
+	width_y: float
+	v_x: float 
+	v_y: float
+	pos_x: float
+	pos_y: float
+	t_init: float
--- tests/model_class.py
+++ tests/model_class.py
@@ -1,1 +1,9 @@
-from model import Model
+from model import Model, Blob
+
+tmp = Model(Nx=100, Ny=100, Lx=100, Ly=100, dt=0.01, T=1)
+
+print(tmp.Nx)
+
+blob = Blob(1,1,1,1,1,1,1,1,1)
+
+print(blob)
