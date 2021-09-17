79e35a1a2f7326b698a90f22dc8201a7ed6013d1
Decristoforo <gregor.decristoforo@uit.no>
2021-09-17 15:48:13 +0200

    listed model methods

--- model/model.py
+++ model/model.py
@@ -1,3 +1,5 @@
+from .blobs import Blob
+
 class Model:
     def __init__(self, Nx, Ny, Lx, Ly, dt, T):
         self.Nx = Nx
@@ -6,3 +8,15 @@ class Model:
         self.Ly = Ly
         self.dt = dt
         self.T = T
+
+    def sample_blobs(self):
+        raise NotImplementedError(self.__class__.__name__ + '.sample_blobs')
+
+    def show_model(self):
+        raise NotImplementedError(self.__class__.__name__ + '.show_model')
+
+    def set_diagnostics(self):
+        raise NotImplementedError(self.__class__.__name__ + '.set_diagnostics')
+
+    def integrate(self):
+        raise NotImplementedError(self.__class__.__name__ + '.integrate')
--- tests/model_class.py
+++ tests/model_class.py
@@ -2,6 +2,8 @@ from model import Model, Blob
 
 tmp = Model(Nx=100, Ny=100, Lx=100, Ly=100, dt=0.01, T=1)
 
+tmp.sample_blobs()
+
 print(tmp.Nx)
 
 blob = Blob(1,1,1,1,1,1,1,1,1)
