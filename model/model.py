987d93c86afe55ffda512772f276f6833180c897
Decristoforo <gregor.decristoforo@uit.no>
2021-09-20 11:02:48 +0200

    added blob attributes to sample_blobs

--- model/model.py
+++ model/model.py
@@ -1,4 +1,5 @@
 from .blobs import Blob
+import numpy as np
 
 class Model:
     def __init__(self, Nx, Ny, Lx, Ly, dt, T):
@@ -8,8 +9,43 @@ class Model:
         self.Ly = Ly
         self.dt = dt
         self.T = T
+        self.__blobs = []
+        self.__dissipation = 'None'
 
-    def sample_blobs(self):
+    def sample_blobs(self, 
+                    num_blobs, 
+                    shape = 'gauss', 
+                    A_dist='exp', 
+                    W_dist='exp', 
+                    vx_dist='deg', 
+                    vy_dist='deg' 
+                ):
+
+        __amp = np.random.exponential(scale=1.0, size=num_blobs)
+        __width = np.random.exponential(scale=1.0, size=num_blobs)
+        __posx = np.zeros(num_blobs)
+        __posy = np.random.uniform(low=0.0, high=self.Ly, size=num_blobs)
+        __vx = np.ones(num_blobs)
+        __vy = np.ones(num_blobs)
+        __t_tinit = np.random.uniform(low=0, high=self.T, size=num_blobs)
+
+        # sort blobs by __t_init
+        __t_tinit = np.sort(__t_tinit)
+
+        for i in range(num_blobs):
+            self.__blobs.append(Blob(id = i,
+                        amplitude=__amp[i],
+                        width_x=__width[i],
+                        width_y = __width[i],
+                        v_x=__vx[i],
+                        v_y=__vy[i],
+                        pos_x=__posx[i],
+                        pos_y = __posy[i],
+                        t_init=__t_tinit[i]
+                        ))
+
+
+    def set_dissipation(self):
         raise NotImplementedError(self.__class__.__name__ + '.sample_blobs')
 
     def show_model(self):
--- tests/model_class.py
+++ tests/model_class.py
@@ -2,7 +2,7 @@ from model import Model, Blob
 
 tmp = Model(Nx=100, Ny=100, Lx=100, Ly=100, dt=0.01, T=1)
 
-tmp.sample_blobs()
+tmp.sample_blobs(num_blobs=2)
 
 print(tmp.Nx)
 
