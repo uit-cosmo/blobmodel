534009d49893fc033f4ec88ace22cfff57a3a2e0
Decristoforo <gregor.decristoforo@uit.no>
2021-09-24 11:37:11 +0200

    update .gitignore

--- tests/model_class.py
+++ tests/model_class.py
@@ -2,10 +2,10 @@ from blobmodel import Model, Blob
 
 bm = Model(Nx=200, Ny=100, Lx=10, Ly=10, dt=0.1, T=10, blob_shape='gauss', t_drain=2)
 
-#tmp.sample_blobs(num_blobs=1000, A_dist='deg',W_dist='deg', vx_dist='deg',vy_dist='zeros')
+#bm.sample_blobs(num_blobs=1000, A_dist='deg',W_dist='deg', vx_dist='deg',vy_dist='zeros')
 
 bm.sample_blobs(num_blobs=100)
 
-#tmp.integrate(file_name='2d_blobs.nc' ,speed_up=False, truncation_Lx = 1)
+#bm.integrate(file_name='2d_blobs.nc' ,speed_up=False, truncation_Lx = 1)
 
 bm.show_model(interval=100)#), save = False)
