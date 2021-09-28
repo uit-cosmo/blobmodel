256d8f4559b9a856d60cbada5c474b63dc2daf01
Decristoforo <gregor.decristoforo@uit.no>
2021-09-28 11:16:53 +0200

    add examples

---
+++ examples/save_data.py
@@ -0,0 +1,10 @@
+from blobmodel import Model
+
+bm = Model(Nx=100, Ny=50, Lx=10, Ly=10, dt=0.1, T=20, blob_shape='exp')
+
+bm.sample_blobs(num_blobs=1000)
+
+# save data as nc file
+# use speedup option with blob truncated after propagating length 2*Lx
+bm.integrate(file_name='example.nc' ,speed_up=True, truncation_Lx = 2)
+
