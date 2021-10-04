e04e7cb94e299ad7a697a00244a4848c9a6f10c1
gregordecristoforo <gregor.decristoforo@gmail.com>
2021-10-04 11:54:45 +0200

    readable blob discretisation

--- blobmodel/blobs.py
+++ blobmodel/blobs.py
@@ -5,7 +5,7 @@ import numpy as np
 @dataclass
 class Blob():
     '''
-    A single blob 
+    A single blob
     '''
     id: int
     blob_shape: str
@@ -20,34 +20,46 @@ class Blob():
     t_drain: float
 
     def discretize_blob(self, x, y, t):
-        ''' 
+        '''
         Discretize blob on grid
         The following blob shapes are implemented:
                 gauss: 2D gaussian function
                 exp: one sided exponential in x and gaussian in y
         '''
         if(self.blob_shape == 'gauss'):
-            return self.amplitude * np.exp(-(t-self.t_init)/self.t_drain)\
-                * np.exp(-((x-self.pos_x - self.v_x*(t-self.t_init))**2/(2*self.width_x**2)))\
-                * np.exp(-((y-self.pos_y - self.v_y*(t-self.t_init))**2/(2*self.width_y**2)))\
-                * np.heaviside(t-self.t_init, 1)
+            return self.amplitude\
+                * self.__drain(t)\
+                * self.__x_shape_gauss(x, t)\
+                * self.__y_shape_gauss(y, t)\
+                * self.__blob_arrival(t)
         elif(self.blob_shape == 'exp'):
-            return self.amplitude * np.exp(-(t-self.t_init)/self.t_drain)\
-                * np.exp(x - self.pos_x - self.v_x*(t-self.t_init))\
-                * np.exp(-((y-self.pos_y - self.v_y*(t-self.t_init))**2/(2*self.width_y**2)))\
-                * np.heaviside(t-self.t_init, 1)*np.heaviside(-1.*(x - self.pos_x - self.v_x*(t-self.t_init)), 1)
+            return self.amplitude\
+                * self.__drain(t)\
+                * self.__x_shape_exp(x, t)\
+                * self.__y_shape_gauss(y, t)\
+                * self.__blob_arrival(t)
         else:
             raise NotImplementedError(
                 self.__class__.__name__ + '.blob shape not implemented')
 
-    def __drain(self):
-        pass
+    def __drain(self, t):
+        return np.exp(-(t-self.t_init)/self.t_drain)
 
-    def __X_variable(self):
-        pass
+    def __blob_arrival(self, t):
+        return np.heaviside(t-self.t_init, 1)
 
-    def __get_y_blob_pos(self):
-        pass
+    def __x_shape_gauss(self, x, t):
+        return np.exp(-((x - self.__get_x_blob_pos(t))**2/(2*self.width_x**2)))
 
-    def __get_x_blob_pos(self):
-        pass
+    def __x_shape_exp(self, x, t):
+        return np.exp(x - self.__get_x_blob_pos(t))\
+            * np.heaviside(-1.*(x - self.__get_x_blob_pos(t)), 1)
+
+    def __y_shape_gauss(self, y, t):
+        return np.exp(-((y-self.__get_y_blob_pos(t))**2/(2*self.width_y**2)))
+
+    def __get_x_blob_pos(self, t):
+        return self.pos_x + self.v_x*(t-self.t_init)
+
+    def __get_y_blob_pos(self, t):
+        return self.pos_y + self.v_y*(t-self.t_init)
