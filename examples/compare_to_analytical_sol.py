dfd47020e7514afd427cf410cd8765f86ce9d58e
gregordecristoforo <gregor.decristoforo@gmail.com>
2021-10-07 10:12:48 +0200

    change compare_to_analytical_sol.py
    return value of integrate() is used instead of reading in *nc file.
    The old method leads to confusion since old *nc files might be
    loaded in

--- examples/compare_to_analytical_sol.py
+++ examples/compare_to_analytical_sol.py
@@ -4,17 +4,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-if not os.path.isfile('./profile_comparison.nc'):
-    tmp = Model(Nx=100, Ny=1, Lx=10, Ly=0, dt=1, T=1000,
-                blob_shape='exp', t_drain=2, periodic_y=False)
-    tmp.sample_blobs(num_blobs=10000, A_dist='deg',
-                     W_dist='deg', vx_dist='deg', vy_dist='zeros')
-    tmp.integrate(file_name='profile_comparison.nc',
-                  speed_up=True, truncation_Lx=1)
+tmp = Model(Nx=100, Ny=1, Lx=10, Ly=0, dt=1, T=1000,
+            blob_shape='exp', t_drain=2, periodic_y=False)
+tmp.sample_blobs(num_blobs=10000, A_dist='deg',
+                    W_dist='deg', vx_dist='deg', vy_dist='zeros')
+ds = tmp.integrate(file_name='profile_comparison.nc',
+                speed_up=True, truncation_Lx=1)
 
 
-def plot_convergence_to_analytical_solution():
-    ds = xr.open_dataset('profile_comparison.nc')
+def plot_convergence_to_analytical_solution(ds):
 
     x = np.linspace(0, 10, 100)
     t_p = 1
@@ -33,4 +31,4 @@ def plot_convergence_to_analytical_solution():
     plt.show()
 
 
-plot_convergence_to_analytical_solution()
+plot_convergence_to_analytical_solution(ds)
