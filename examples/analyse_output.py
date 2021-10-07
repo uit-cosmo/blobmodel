3d79151fe71a4f141657cdd0a17e095c1ec45ba9
gregordecristoforo <gregor.decristoforo@gmail.com>
2021-10-07 12:28:15 +0200

    use black ormatting

--- examples/analyse_output.py
+++ examples/analyse_output.py
@@ -3,15 +3,15 @@ import xarray as xr
 import matplotlib.pyplot as plt
 import os
 
-if not os.path.isfile('./example.nc'):
-    bm = Model(Nx=100, Ny=50, Lx=10, Ly=10, dt=0.1, T=20, blob_shape='exp')
+if not os.path.isfile("./example.nc"):
+    bm = Model(Nx=100, Ny=50, Lx=10, Ly=10, dt=0.1, T=20, blob_shape="exp")
     bm.sample_blobs(num_blobs=1000)
-    bm.integrate(file_name='example.nc', speed_up=True, truncation_Lx=2)
+    bm.integrate(file_name="example.nc", speed_up=True, truncation_Lx=2)
 
 # use xarray to open output
-ds = xr.open_dataset('example.nc')
+ds = xr.open_dataset("example.nc")
 
 # use xarray syntax for analysing output
 # for example
-ds['n'].isel(y=0).mean(dim=('t')).plot()
+ds["n"].isel(y=0).mean(dim=("t")).plot()
 plt.show()
--- tests/test_analytical.py
+++ tests/test_analytical.py
@@ -2,26 +2,38 @@ from blobmodel import Model
 import xarray as xr
 import numpy as np
 
-tmp = Model(Nx=100, Ny=1, Lx=10, Ly=0, dt=1, T=1000,
-            blob_shape='exp', t_drain=2, periodic_y=False)
-tmp.sample_blobs(num_blobs=10000, A_dist='deg', W_dist='deg',
-                 vx_dist='deg', vy_dist='zeros')
-tmp.integrate(file_name='test_analytical.nc', speed_up=True, truncation_Lx=1)
+tmp = Model(
+    Nx=100,
+    Ny=1,
+    Lx=10,
+    Ly=0,
+    dt=1,
+    T=1000,
+    blob_shape="exp",
+    t_drain=2,
+    periodic_y=False,
+)
+tmp.sample_blobs(
+    num_blobs=10000, A_dist="deg", W_dist="deg", vx_dist="deg", vy_dist="zeros"
+)
+tmp.integrate(file_name="test_analytical.nc", speed_up=True, truncation_Lx=1)
 
 
 def test_convergence_to_analytical_solution():
-    ds = xr.open_dataset('test_analytical.nc')
-    model_profile = ds.n.isel(y=0).mean(dim=('t'))
+    ds = xr.open_dataset("test_analytical.nc")
+    model_profile = ds.n.isel(y=0).mean(dim=("t"))
 
     x = np.linspace(0, 10, 100)
     t_p = 1
-    t_w = 1/10
+    t_w = 1 / 10
     amp = 1
     v_p = 1.0
     t_loss = 2.0
-    t_d = t_loss*t_p/(t_loss+t_p)
+    t_d = t_loss * t_p / (t_loss + t_p)
 
-    analytical_profile = 1/np.sqrt(np.pi) * t_d/t_w * amp * np.exp(-x/(v_p*t_loss))
+    analytical_profile = (
+        1 / np.sqrt(np.pi) * t_d / t_w * amp * np.exp(-x / (v_p * t_loss))
+    )
 
     error = np.mean(abs(model_profile.values - analytical_profile))
 
