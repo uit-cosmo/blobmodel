853d4c40bf025395ea7dbb4bd0d9de01b950f2c8
gregordecristoforo <gregor.decristoforo@gmail.com>
2021-10-12 08:44:25 +0200

    show_model takes Model output as input

--- blobmodel/plotting.py
+++ blobmodel/plotting.py
@@ -2,22 +2,23 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
-from tqdm import tqdm
-from blobmodel import Model
+import xarray as xr
 
 
 def show_model(
-    model: Model,
+    ds: xr.Dataset,
     interval: int = 100,
     save: bool = False,
     gif_name: str = "2d_blobs.gif",
     fps: int = 10,
 ) -> None:
     """
-        Show animation of Model
+        Show animation of Model output
 
         Parameters
         ----------
+        ds: xarray Dataset,
+            Model data
         interval: int, optional
             time interval between frames in ms
         save: bool, optional
@@ -34,20 +35,16 @@ def show_model(
 
     frames = []
 
-    __xx, __yy = np.meshgrid(model.x, model.y)
-
-    for t in tqdm(model.t, desc="Creating frames for animation"):
-        curVals = np.zeros(shape=(model.Ny, model.Nx))
-        for b in model.get_blobs():
-            curVals += b.discretize_blob(
-                x=__xx, y=__yy, t=t, periodic_y=model.periodic_y, Ly=model.Ly
-            )
-        frames.append(curVals)
+    for timestep in ds.t.values:
+        frame = ds["n"].sel(t=timestep).values
+        frames.append(frame)
 
     cv0 = frames[0]
     im = ax.imshow(cv0, origin="lower")
     fig.colorbar(im, cax=cax)
     tx = ax.set_title("t = 0")
+
+    dt = ds.t.values[1] - ds.t.values[0]
 
     def animate(i: int) -> None:
         arr = frames[i]
@@ -55,9 +52,9 @@ def show_model(
         vmin = np.min(arr)
         im.set_data(arr)
         im.set_clim(vmin, vmax)
-        tx.set_text(f"t = {i*model.dt:.2f}")
+        tx.set_text(f"t = {i*dt:.2f}")
 
-    ani = FuncAnimation(fig, animate, frames=model.t.size, interval=interval)
+    ani = FuncAnimation(fig, animate, frames=ds["t"].values.size, interval=interval)
     if save:
         ani.save(gif_name, writer="ffmpeg", fps=fps)
     plt.show()
--- examples/show_animation.py
+++ examples/show_animation.py
@@ -4,5 +4,8 @@ bm = Model(Nx=100, Ny=50, Lx=10, Ly=10, dt=0.1, T=20, blob_shape="gauss")
 
 bm.sample_blobs(num_blobs=1000)
 
+# create data
+ds = bm.integrate(speed_up=True, truncation_Lx=2)
+
 # show animation and save as gif
-show_model(model=bm, interval=100, save=True, gif_name="example.gif", fps=10)
+show_model(ds=ds, interval=100, save=True, gif_name="example.gif", fps=10)
