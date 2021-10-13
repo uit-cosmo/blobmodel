88399a2aa1b774a2d1ce9240e5c8c2915c23f268
gregordecristoforo <gregor.decristoforo@gmail.com>
2021-10-13 08:42:04 +0200

    make examples compatible with stochasticality branch

--- examples/analyse_output.py
+++ examples/analyse_output.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 import os
 
 if not os.path.isfile("./example.nc"):
-    bm = Model(Nx=100, Ny=50, Lx=10, Ly=10, dt=0.1, T=20, blob_shape="exp")
-    bm.sample_blobs(num_blobs=1000)
+    bm = Model(
+        Nx=100, Ny=50, Lx=10, Ly=10, dt=0.1, T=20, blob_shape="exp", num_blobs=1000
+    )
     bm.integrate(file_name="example.nc", speed_up=True, truncation_Lx=2)
 
 # use xarray to open output
