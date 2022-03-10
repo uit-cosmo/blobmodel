3068830810f313bf32b5b0a1a7260e3ab6fcf625
gregordecristoforo <gregor.decristoforo@gmail.com>
2022-03-10 11:18:15 +0100

    plotting funciton supports 1d
    thorough debugging still needed

--- blobmodel/plotting.py
+++ blobmodel/plotting.py
@@ -31,24 +31,16 @@ def show_model(
         set fps for gif
     """
     fig = plt.figure()
-    ax = fig.add_subplot(111)
-    div = make_axes_locatable(ax)
-    cax = div.append_axes("right", "5%", "5%")
-
-    frames = []
-
-    for timestep in dataset.t.values:
-        frame = dataset[variable].sel(t=timestep).values
-        frames.append(frame)
-
-    cv0 = frames[0]
-    im = ax.imshow(cv0, origin="lower")
-    fig.colorbar(im, cax=cax)
-    tx = ax.set_title("t = 0")
 
     dt = dataset.t.values[1] - dataset.t.values[0]
 
-    def animate(i: int) -> None:
+    def animate_1d(i: int) -> None:
+        x = dataset.x
+        y = frames[i]
+        line.set_data(x, y)
+        plt.title(f"t = {i*dt:.2f}")
+
+    def animate_2d(i: int) -> None:
         arr = frames[i]
         vmax = np.max(arr)
         vmin = np.min(arr)
@@ -56,9 +48,32 @@ def show_model(
         im.set_clim(vmin, vmax)
         tx.set_text(f"t = {i*dt:.2f}")
 
-    ani = animation.FuncAnimation(
-        fig, animate, frames=dataset["t"].values.size, interval=interval
-    )
+    frames = []
+
+    for timestep in dataset.t.values:
+        frame = dataset[variable].sel(t=timestep).values
+        frames.append(frame)
+
+    if dataset.y.size == 1:
+        fig = plt.figure()
+        ax = plt.axes(xlim=(0, dataset.x[-1]), ylim=(0, dataset.n.max()))
+        tx = ax.set_title("t = 0")
+        (line,) = ax.plot([], [], lw=2)
+        ani = animation.FuncAnimation(
+            fig, animate_1d, frames=dataset["t"].values.size, interval=interval
+        )
+    else:
+        ax = fig.add_subplot(111)
+        tx = ax.set_title("t = 0")
+        div = make_axes_locatable(ax)
+        cax = div.append_axes("right", "5%", "5%")
+        cv0 = frames[0]
+        im = ax.imshow(cv0, origin="lower")
+        fig.colorbar(im, cax=cax)
+        ani = animation.FuncAnimation(
+            fig, animate_2d, frames=dataset["t"].values.size, interval=interval
+        )
+
     if save:
         ani.save(gif_name, writer="ffmpeg", fps=fps)
     plt.show()
--- examples/1d_animation.py
+++ examples/1d_animation.py
@@ -1,10 +1,5 @@
 from blobmodel import Model, show_model, DefaultBlobFactory
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib import animation
 
-# here you can define your custom parameter distributions
 bf = DefaultBlobFactory(A_dist="deg", W_dist="deg", vx_dist="deg", vy_dist="zeros")
 
 bm = Model(
@@ -22,38 +17,4 @@ bm = Model(
 )
 
 ds = bm.make_realization(speed_up=True, error=1e-2)
-print(ds)
-
-
-# First set up the figure, the axis, and the plot element we want to animate
-fig = plt.figure()
-ax = plt.axes(xlim=(0, ds.x[-1]), ylim=(0, ds.n.max()))
-(line,) = ax.plot([], [], lw=2)
-
-frames = []
-
-for timestep in ds.t.values:
-    frame = ds["n"].sel(t=timestep).values
-    frames.append(frame)
-
-tx = ax.set_title("t = 0")
-
-dt = ds.t.values[1] - ds.t.values[0]
-# animation function.  This is called sequentially
-def animate(i: int) -> None:
-    x = ds.x
-    y = frames[i]
-    line.set_data(x, y)
-    plt.title(f"t = {i*dt:.2f}")
-
-
-plt.xlabel("x")
-plt.ylabel("n")
-# call the animator.  blit=True means only re-draw the parts that have changed.
-anim = animation.FuncAnimation(
-    fig,
-    animate,
-    frames=ds["t"].values.size,
-    interval=100,
-)
-plt.show()
+show_model(dataset=ds, interval=100, save=True, gif_name="example.gif", fps=10)
