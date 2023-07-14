59fc685108cf907a6806f051283f9d83072b2f14
gregordecristoforo <gregor.decristoforo@gmail.com>
2023-07-14 15:20:11 +0200

    mock blobmodel

--- docs/conf.py
+++ docs/conf.py
@@ -30,11 +30,12 @@ templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 autodoc_mock_imports = ["numpy"]
 autodoc_mock_imports = ["xarray"]
+autodoc_mock_imports = ["blobmodel"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 html_favicon = "./logo.png"
