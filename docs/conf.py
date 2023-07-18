1f6fe17e3f69d828b56a750f8330f1b2f914a37d
gregordecristoforo <gregor.decristoforo@gmail.com>
2023-07-18 10:01:41 +0200

    add relative import to install docs on readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@ import sys
 import blobmodel
 
 sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../blobmodel"))
 
 
 # -- General configuration ---------------------------------------------------
