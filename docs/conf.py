0d0535e399b923606f2de709c40e6603bcbf9a9d
gregordecristoforo <gregor.decristoforo@gmail.com>
2023-07-18 10:12:54 +0200

    add requirements.txt

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,6 @@ import sys
 import blobmodel
 
 sys.path.insert(0, os.path.abspath(".."))
-sys.path.insert(0, os.path.abspath("../blobmodel"))
 
 
 # -- General configuration ---------------------------------------------------
